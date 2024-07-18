d969bd5513b61fd60d28fb1b364ade057500262f
Robert <robersargsyan2023@gmail.com>
2024-07-18 23:00:02 +0300

    d

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,13 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+// const { defineConfig } = require('@vue/cli-service')
+//
+//
+// module.exports = defineConfig({
+//   transpileDependencies: true,
+// npm run build
+// cd dist git init git add -A git commit -m 'deploy'
+// git push -f git@github.com:Hrayrdev/an-ru.git
+// })
+
+module.exports = {
+  publicPath: '/an-ru/'
+}
